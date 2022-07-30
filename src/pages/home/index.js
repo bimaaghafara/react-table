4ee744b580b97aec6e7d89a4c8f1a86d1b97a398
bimaaghafara <bimaa7@gmail.com>
2022-07-30 12:49:03 +0700

    implement DataGrid

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Button, Box, Typography, TextField, MenuItem } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
 import { setFilters, resetFilters } from '../../redux/actions';
 import { useDispatch, useSelector } from "react-redux";
 import { useDebounce } from "../../hooks/useDebounce";
@@ -10,12 +11,23 @@ export default function Home() {
   const dispatch = useDispatch();
   const filtersState = useSelector((state) => state.FiltersReducer);
   const filtersStateDebounced = useDebounce(filtersState, 500);
+  const [loading, setLoading] = useState(false);
+  const [rows, setRows] = useState([]);
 
   useEffect(() => {
-    const url = 'https://randomuser.me/api/?page=1&pageSize=10&results=10'
+    setLoading(true);
+    const url = 'https://randomuser.me/api/?page=1&pageSize=10&results=10';
     axios.get(url)
-      .then(res => console.log(res.data))
-      .catch(err => console.log(err))
+      .then(res => {
+        setLoading(false);
+        setRows(res.data.results.map(e => ({
+          ...e, id: [e.id.value, e.id.value].join('_')
+        })));
+      })
+      .catch(err => {
+        setLoading(false);
+        console.log(err);
+      })
   }, [filtersStateDebounced]);
   
   const renderTableFilters = () => {
@@ -65,10 +77,65 @@ export default function Home() {
     );
   }
 
+  const renderTable = () => {
+    const columns = [
+      {
+        width: 150,
+        field: "username",
+        headerName: "Username",
+        valueGetter: ({ row }) => row.login.username,
+      },
+      {
+        width: 200,
+        field: "name",
+        headerName: "Name",
+        valueGetter: ({ row }) => [row.name.first, row.name.last].join(' ')
+      },
+      {
+        width: 250,
+        field: "email",
+        headerName: "Email",
+      },
+      {
+        width: 150,
+        field: "gender",
+        headerName: "Gender",
+      },
+      {
+        width: 250,
+        field: "registeredDate",
+        headerName: "Registered Date",
+        valueGetter: ({ row }) => new Date(row.registered.date).toLocaleString()
+      },
+    ]
+
+    return (
+      <Box sx={{ height: "580px", width: "100%", mt: 2 }}>
+        <DataGrid
+          loading={loading}
+          rows={rows}
+          columns={columns}
+          disableSelectionOnClick
+          disableColumnMenu
+          sortingMode="server"
+          onSortModelChange={(e, details) => console.log(e, details)}
+          paginationMode="server"
+          page={0}
+          pageSize={10}
+          rowsPerPageOptions={[10, 25, 50, 100]}
+          rowCount={999}
+          onPageChange={(e) => console.log(e)}
+          onPageSizeChange={(e) => console.log(e)}
+        />
+      </Box>
+    );
+  }
+
   return (
     <Box sx={sx.root}>
       <Typography variant="h5">React Table</Typography>
       {renderTableFilters()}
+      {renderTable()}
     </Box>
   )
 }
